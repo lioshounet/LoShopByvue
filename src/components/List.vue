8625ad77cf91c946d209f333ea81e98732ea94a6
lioshounet <1226741725@qq.com>
2021-08-20 10:52:16 +0800

    全品牌上架，json未优化

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,15 +4,14 @@
     <ul>
       <li @click="missjane()">珍妮小姐</li>
       <li @click="autumnforest()">秋日森林</li>
-      <li>789</li>
-      <li>123</li>
-      <li>456</li>
-      <li>789</li>
-      <li>456</li>
-      <li>789</li>
-      <li>123</li>
-      <li>456</li>
-      <li>789</li>
+      <li @click="honeyhoney()">Honeyhoney</li>
+      <li @click="arconasdream()">阿尔可纳之梦</li>
+      <li @click="badoni()">巴朵妮</li>
+      <li @click="brokendoll()">崩坏人偶</li>
+      <li @click="pandabamboo()">熊猫梦竹</li>
+      <li @click="anthonycake()">安东尼蛋糕</li>
+      <li @click="aprilamwell()">四月安</li>
+      <li @click="lancelot()">兰斯洛特</li>
     </ul>
   </div>
 </template>
@@ -28,6 +27,30 @@ export default {
     },
     autumnforest: function () {
       Msg.$emit("showwhat", "autumnforest");
+    },
+    honeyhoney: function () {
+      Msg.$emit("showwhat", "honeyhoney");
+    },
+    arconasdream: function () {
+      Msg.$emit("showwhat", "arconasdream");
+    },
+    badoni: function () {
+      Msg.$emit("showwhat", "badoni");
+    },
+    brokendoll: function () {
+      Msg.$emit("showwhat", "brokendoll");
+    },
+    pandabamboo: function () {
+      Msg.$emit("showwhat", "pandabamboo");
+    },
+    anthonycake: function () {
+      Msg.$emit("showwhat", "anthonycake");
+    },
+    aprilamwell: function () {
+      Msg.$emit("showwhat", "aprilamwell");
+    },
+    lancelot: function () {
+      Msg.$emit("showwhat", "lancelot");
     },
   },
 };
